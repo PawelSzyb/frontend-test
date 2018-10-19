010161f51ca84b9cf8d0eb2ec5b7a765f0a4f584
Paweł Szybaj <pawelszybaj@wp.pl>
2018-10-19 14:48:49 +0200

    Implementing react-router-dom and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+        </div>
+      </Router>
+    );
   }
 }
 
