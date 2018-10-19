ac65981b4ff74d5ae84fd54ba350d19132a4d52a
Paweł Szybaj <pawelszybaj@wp.pl>
2018-10-19 15:11:46 +0200

    quotation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
+import Quotation from "./components/quotation/Quotation";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navbar />
+          <Route exact path="/quotation" component={Quotation} />
         </div>
       </Router>
     );
---
+++ src/components/quotation/Quotation.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Quotation.scss";
+
+function Quotation() {
+  return (
+    <div className="quote-wrapper">
+      <h1>Quotation</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptas
+        illum maxime saepe pariatur porro perspiciatis eum doloremque nemo
+        culpa.
+      </p>
+      <div class="blockquote-footer">
+        Someone famous in <cite title="Source Title">Source Title</cite>
+      </div>
+    </div>
+  );
+}
+
+export default Quotation;
